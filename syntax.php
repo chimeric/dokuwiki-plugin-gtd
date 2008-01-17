84e8aff6e332d5d6a3facb61cb78ca2ee6142dff
Michael Klier <chi@chimeric.de>
2008-01-17 16:33:27 +0100

    added global expiries
    darcs-hash:20080117153327-23886-6584a6f62204f3b776de758c826e9e5152f00379.gz

--- syntax.php
+++ syntax.php
@@ -11,7 +11,7 @@ if(!defined('DOKU_INC')) die();
 if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 require_once(DOKU_PLUGIN.'syntax.php');
 
-if(!defined('DW_LF')) define('DW_LF',"\n");
+if(!defined('DOKU_LF')) define('DOKU_LF',"\n");
  
 /**
  * All DokuWiki plugins to extend the parser/rendering mechanism
@@ -26,18 +26,13 @@ class syntax_plugin_gtd extends DokuWiki_Syntax_Plugin {
         return array(
             'author' => 'Michael Klier',
             'email'  => 'chi@chimeric.de',
-            'date'   => '2007-08-10',
+            'date'   => '2008-01-17',
             'name'   => 'GTD (Getting Things Done)',
             'desc'   => 'Implements a ToDo List following the principles of GTD.',
             'url'    => 'http://www.chimeric.de/projects/dokuwiki/plugin/gtd',
         );
     }
  
-    /**
-     * Syntax Type
-     *
-     * Needs to return one of the mode types defined in $PARSER_MODES in parser.php
-     */
     function getType()  { return 'substition'; }
     function getPType() { return 'block'; }
     function getSort()  { return 320; }
@@ -45,19 +40,41 @@ class syntax_plugin_gtd extends DokuWiki_Syntax_Plugin {
     /**
      * Connect pattern to lexer
      */
-    function connectTo($mode) { $this->Lexer->addSpecialPattern('<gtd>.+?</gtd>',$mode,'plugin_gtd'); }
+    function connectTo($mode) { $this->Lexer->addSpecialPattern('<gtd.*?>.+?</gtd>',$mode,'plugin_gtd'); }
  
     /**
      * Handle the match
      */
     function handle($match, $state, $pos, &$handler) {
-        $match = substr($match, 5, -6);
-        $todos = $this->_todo2array($match);
-        return ($todos);
+
+        // check for modified expiries
+        if(preg_match("#<gtd(.*?)>#", $match, $params)) {
+            if(!empty($params[1])) {
+                $expiries = array();
+                if(preg_match_all("#((warn|due)=(\d+))#", $params[1], $opts)) {
+                    for($i=0;$i<count($opts[2]);$i++) {
+                        if($opts[2][$i] == 'warn' or $opts[2][$i] == 'due') {
+                            $expiries[$opts[2][$i]] = $opts[3][$i];
+                        }
+                    }
+                }
+            }
+        }
+
+        // set global expiries if not given yet
+        if(!empty($expiries)) {
+            $expiries['warn'] = 5;
+            $expiries['due']  = 2;
+        }
+
+        $match    = preg_replace('#<gtd.*?>|</gtd>#', '', $match);
+        $todolist = $this->_todo2array($match, $expiries);
+
+        return ($todolist);
     }
 
     /**
-     * Create output
+     * Creates the output
      *
      * @author Michael Klier <chi@chimeric.de>
      */
@@ -75,7 +92,7 @@ class syntax_plugin_gtd extends DokuWiki_Syntax_Plugin {
      *
      * @author Michael Klier <chi@chimeric.de>
      */
-    function _todo2array($data) {
+    function _todo2array($data, $expiries) {
 
         $todolist = array();
 
@@ -121,11 +138,11 @@ class syntax_plugin_gtd extends DokuWiki_Syntax_Plugin {
             // filter date
             if(preg_match("#\b\d{4}-\d{2}-\d{2}\b#", $params, $match)) {
                 $todo['date'] = $match[0];
-                $todo['priority'] = $this->_get_priority($todo['date']);
+                $todo['priority'] = $this->_get_priority($todo['date'], $expiries);
                 $params = trim(str_replace($match[0], '', $params));
             } elseif(preg_match("#\d{2}-\d{2}#", $params, $match)) {
                 $todo['date'] = date('Y') . '-' . $match[0];
-                $todo['priority'] = $this->_get_priority($todo['date']);
+                $todo['priority'] = $this->_get_priority($todo['date'], $expiries);
                 $params = trim(str_replace($match[0], '', $params));
             }
 
@@ -159,34 +176,34 @@ class syntax_plugin_gtd extends DokuWiki_Syntax_Plugin {
         $renderer->interwiki = getInterwiki();
 
         foreach($todolist as $context => $todos) {
-            $out .= '<div class="plugin_gtd_box">' . DW_LF;
-            $out .= '<h2 class="plugin_gtd_context">' . htmlspecialchars($context) . '</h2>' . DW_LF;
-            $out .= '<ul class="plugin_gtd_list">' . DW_LF;
+            $out .= '<div class="plugin_gtd_box">' . DOKU_LF;
+            $out .= '<h2 class="plugin_gtd_context">' . htmlspecialchars($context) . '</h2>' . DOKU_LF;
+            $out .= '<ul class="plugin_gtd_list">' . DOKU_LF;
 
             if(!empty($todolist[$context]['projects'])) {
                 foreach($todolist[$context]['projects'] as $project => $todos) {
-                    $out .= '<li class="plugin_gtd_project"><span class="li plugin_gtd_project">' . htmlspecialchars($project) . '</span>' . DW_LF;
-                    $out .= '<ul class="plugin_gtd_project">' . DW_LF;
+                    $out .= '<li class="plugin_gtd_project"><span class="li plugin_gtd_project">' . htmlspecialchars($project) . '</span>' . DOKU_LF;
+                    $out .= '<ul class="plugin_gtd_project">' . DOKU_LF;
                     foreach($todos as $todo) {
                         $out .= @$this->_todo_xhtml(&$renderer, $todo);
                     }
-                    $out .= '</ul>' . DW_LF;
-                    $out .= '</li>' . DW_LF;
+                    $out .= '</ul>' . DOKU_LF;
+                    $out .= '</li>' . DOKU_LF;
                 }
             }
 
             if(!empty($todolist[$context]['todos'])) {
-                $out .= '<li class="plugin_gtd_project"><span class="li plugin_gtd_project">' . $this->getLang('noproject') . '</span>' . DW_LF;
-                $out .= '<ul class="plugin_gtd_project">' . DW_LF;
+                $out .= '<li class="plugin_gtd_project"><span class="li plugin_gtd_project">' . $this->getLang('noproject') . '</span>' . DOKU_LF;
+                $out .= '<ul class="plugin_gtd_project">' . DOKU_LF;
                 foreach($todolist[$context]['todos'] as $todo) {
                     $out .= @$this->_todo_xhtml(&$renderer, $todo);
                 }
-                $out .= '</ul>' . DW_LF;
-                $out .= '</li>' . DW_LF;
+                $out .= '</ul>' . DOKU_LF;
+                $out .= '</li>' . DOKU_LF;
             }
 
-            $out .= '</ul>' . DW_LF;
-            $out .= '</div>' . DW_LF;
+            $out .= '</ul>' . DOKU_LF;
+            $out .= '</div>' . DOKU_LF;
         }
 
         return ($out);
@@ -203,13 +220,17 @@ class syntax_plugin_gtd extends DokuWiki_Syntax_Plugin {
         // reset doc
         $renderer->doc = '';
 
-        $out .= '<li class="plugin_gtd_item"><div class="li">' . DW_LF;
+        $out .= '<li class="plugin_gtd_item ';
+        if(isset($todo['date'])) {
+            if(!$todo['done']) {
+                $out .= 'plugin_gtd_' . $todo['priority'];
+            }
+        }
+        $out .= '"><div class="li">' . DOKU_LF;
 
 
         if(isset($todo['date'])) {
-            $out .= '<div class="plugin_gtd_date"><span class="';
-            if(!$todo['done']) $out .= 'plugin_gtd_' . $todo['priority'];
-            $out .= '">' . $todo['date'] . '</span></div>' . DW_LF;
+            $out .= '<div class="plugin_gtd_date">' . $todo['date'] . '</span>' . DOKU_LF;
         }
 
         $out .= '<div class="plugin_gtd_desc">'; 
@@ -232,25 +253,27 @@ class syntax_plugin_gtd extends DokuWiki_Syntax_Plugin {
 
         if($todo['done']) $out .= '</del>';
 
-        $out .= '</div>' . DW_LF;
-
-
-        $out .= '</div></li>' . DW_LF;
+        $out .= '</div>' . DOKU_LF;
+        $out .= '</div></li>' . DOKU_LF;
 
         return ($out);
     }
 
     /**
-     * calculates the priority by a given date string
+     * Calculates the priority by a given date string
+     * and returns the CSS class to use
      *
      * @author Michael Klier <chi@chimeric.de>
      */
-    function _get_priority($date) {
+    function _get_priority($date, $expiries) {
+
         $ctime = time();
         list($y,$m,$d) = explode('-', $date);
-        $dtime = mktime(0, 0, 0, $m, $d, $y);
-        if($ctime >= $dtime) return 'over';
-        if(($dtime - $ctime) >= 60*60*24*7) return 'upcoming';
-        return 'current';
+        $etime = mktime(0, 0, 0, $m, $d, $y);
+
+        if(($etime - $ctime) < 0) return 'pass';
+        if(($etime - $ctime) <= 60*60*24*$expiries['due']) return 'due';
+        if(($etime - $ctime) <= 60*60*24*$expiries['warn']) return 'warn';
+        return 'upco';
     }
 }
